40b390314463e762da2ede2d1a2b990b01d31f72
Alyssa Evans <alyda@me.com>
2025-09-20 22:55:35 +0000

    beans/abc/directive

--- src/beans/mod.rs
+++ src/beans/mod.rs
@@ -1,1 +1,11 @@
+mod abc {
+    /// required behavior for a Directive
+    trait Entry {
+        fn get_date(&self) -> time::Date;
+        fn get_meta(&self) {
+            todo!()
+        }
+    }
+}
+
 mod flags;
